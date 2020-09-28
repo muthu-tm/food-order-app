410c3042872ec472a11c7e4c3f10d380597b797a
Muthu Thavamani <muthucs@outlook.com>
2020-09-28 17:38:11 +0530

    minor ui changes

--- lib/screens/orders/OrderSuccessWidget.dart
+++ lib/screens/orders/OrderSuccessWidget.dart
@@ -1,81 +1,94 @@
 import 'package:chipchop_buyer/screens/home/HomeScreen.dart';
+import 'package:chipchop_buyer/screens/utils/CustomColors.dart';
 import 'package:flutter/material.dart';
 
 class OrderSuccessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(16.0))),
-        content: Container(
-          height: MediaQuery.of(context).size.height / 1.8,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.check_circle_outline,
-                size: 96,
-                color: Color(0xFF10CA88),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(15.0),
+        ),
+      ),
+      content: Container(
+        height: 350,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.check_circle_outline,
+              size: 96,
+              color: CustomColors.green,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                "Your order successfull!",
+                style: TextStyle(fontSize: 20),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Text(
-                  "Your order successfull",
-                  style: TextStyle(fontSize: 20),
-                ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                "Your can track the order status in 'Orders' menu!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Text(
-                  "Your can track the delivery in the Orders section ",
-                  style: TextStyle(fontSize: 16),
-                ),
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4.0),
               ),
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4.0),
+              color: CustomColors.blue,
+              onPressed: () => {},
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 15.0,
+                  horizontal: 10.0,
                 ),
-                color: Color(0xFFF93963),
-                onPressed: () => {},
-                child: Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 15.0,
-                    horizontal: 10.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomeScreen(),
-                                settings: RouteSettings(name: '/'),
-                              ),
-                            );
-                          },
-                          child: Text(
-                            "Continue Shopping",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                              settings: RouteSettings(name: '/'),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          "Track Order",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: CustomColors.white,
+                              fontWeight: FontWeight.bold),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-              FlatButton(
-                child: Text("Go to orders"),
-                onPressed: () {},
-              ),
-            ],
-          ),
+            ),
+            FlatButton(
+              child: Text("Go to Home"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                    settings: RouteSettings(name: '/'),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
+      ),
     );
   }
 }
