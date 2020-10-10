5ada38b5b419611006706ce38268fd65ad81dddf
Muthu Thavamani <muthucs@outlook.com>
2020-10-10 16:27:17 +0530

    custom dialog color changes

--- lib/screens/utils/CustomDialogs.dart
+++ lib/screens/utils/CustomDialogs.dart
@@ -95,7 +95,7 @@ class CustomDialogs {
                         height: 45,
                         child: CircularProgressIndicator(
                           strokeWidth: 3,
-                          backgroundColor: CustomColors.blue,
+                          backgroundColor: CustomColors.alertRed,
                           valueColor:
                               AlwaysStoppedAnimation<Color>(CustomColors.green),
                         ),
@@ -109,51 +109,6 @@ class CustomDialogs {
       },
     );
   }
-
-  // static actionWaiting(BuildContext context) {
-  //   showDialog(
-  //     barrierDismissible: false,
-  //     context: context,
-  //     barrierColor: CustomColors.lightGrey.withOpacity(0.7),
-  //     builder: (BuildContext context) {
-  //       return Container(
-  //         alignment: Alignment.center,
-  //         width: 50,
-  //         height: 50,
-  //         child: Stack(
-  //           children: <Widget>[
-  //             Container(
-  //               width: 45,
-  //               height: 45,
-  //               alignment: Alignment.center,
-  //               child: ClipRRect(
-  //                 child: Image.asset(
-  //                   "images/icons/logo.png",
-  //                   height: 35,
-  //                   width: 35,
-  //                 ),
-  //               ),
-  //             ),
-  //             Positioned(
-  //               top: 0,
-  //               left: 0,
-  //               child: SizedBox(
-  //                 width: 45,
-  //                 height: 45,
-  //                 child: CircularProgressIndicator(
-  //                   strokeWidth: 3,
-  //                   backgroundColor: CustomColors.blue,
-  //                   valueColor:
-  //                       AlwaysStoppedAnimation<Color>(CustomColors.green),
-  //                 ),
-  //               ),
-  //             ),
-  //           ],
-  //         ),
-  //       );
-  //     },
-  //   );
-  // }
 
   static confirm(BuildContext context, String title, String description,
       Function() yesAction, Function() noAction) {
