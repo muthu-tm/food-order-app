fdcb19b9b410c2e074a88f0e3111921e3dd74b4f
madhank93 <maddymadhan946@gmail.com>
2020-10-29 22:53:39 +0530

    feat: product review completed

--- lib/screens/store/ProductDetailsScreen.dart
+++ lib/screens/store/ProductDetailsScreen.dart
@@ -3,6 +3,7 @@ import 'package:chipchop_buyer/db/models/store.dart';
 import 'package:chipchop_buyer/screens/orders/ShoppingCartScreen.dart';
 import 'package:chipchop_buyer/screens/store/CartCounterWidget.dart';
 import 'package:chipchop_buyer/screens/store/ProductFAQsWidget.dart';
+import 'package:chipchop_buyer/screens/store/ProductReviewWidget.dart';
 import 'package:chipchop_buyer/screens/store/StoreProfileWidget.dart';
 import 'package:chipchop_buyer/screens/utils/AsyncWidgets.dart';
 import 'package:chipchop_buyer/screens/utils/CarouselIndicatorSlider.dart';
@@ -296,9 +297,11 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen>
                 Container(
                   child: getStoreDetails(context),
                 ),
-                Container(
+                SingleChildScrollView(
+                  child: ProductReviewWidget(widget.product.uuid),
                 ),
-                SingleChildScrollView(child: ProductFAQsWidget(widget.product.uuid)),
+                SingleChildScrollView(
+                    child: ProductFAQsWidget(widget.product.uuid)),
               ],
             ),
           )
