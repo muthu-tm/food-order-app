c1f2fa2ccc20618297558eaa6d18f4725e0b4a43
madhank93 <maddymadhan946@gmail.com>
2020-12-11 15:43:20 +0530

    fix: adding all filter option

--- lib/screens/store/StoreCategoriesScreen.dart
+++ lib/screens/store/StoreCategoriesScreen.dart
@@ -65,42 +65,74 @@ class _StoreCategoriesScreenState extends State<StoreCategoriesScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 60,
-              padding: const EdgeInsets.all(5.0),
-              child: ListView.builder(
-                primary: true,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: widget.subCategories.length,
-                padding: EdgeInsets.all(5),
-                itemBuilder: (BuildContext context, int index) {
-                  ProductSubCategories _sc = widget.subCategories[index];
-                  return Padding(
-                    padding: EdgeInsets.only(left: 5.0, right: 5),
-                    child: ActionChip(
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Row(
+                  children: [
+                    widget.subCategories.length > 0 ? ActionChip(
                       elevation: 6.0,
-                      backgroundColor: _subCategoryID == _sc.uuid
+                      backgroundColor: _subCategoryID == ""
                           ? CustomColors.green
                           : Colors.white,
                       onPressed: () {
                         setState(() {
-                          _subCategoryID = _sc.uuid;
+                          _subCategoryID = "";
                         });
                       },
                       label: Text(
-                        _sc.name,
+                        "All",
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                         style: TextStyle(
-                            color: _subCategoryID == _sc.uuid
+                            color: _subCategoryID == ""
                                 ? Colors.black54
                                 : CustomColors.black),
                       ),
+                    ) : Container(),
+                    Container(
+                      height: 60,
+                      padding: const EdgeInsets.all(5.0),
+                      child: ListView.builder(
+                        primary: false,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: widget.subCategories.length,
+                        padding: EdgeInsets.all(5),
+                        itemBuilder: (BuildContext context, int index) {
+                          ProductSubCategories _sc =
+                              widget.subCategories[index];
+                          return Padding(
+                            padding: EdgeInsets.only(left: 5.0, right: 5),
+                            child: ActionChip(
+                              elevation: 6.0,
+                              backgroundColor: _subCategoryID == _sc.uuid
+                                  ? CustomColors.green
+                                  : Colors.white,
+                              onPressed: () {
+                                setState(() {
+                                  _subCategoryID = _sc.uuid;
+                                });
+                              },
+                              label: Text(
+                                _sc.name,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    color: _subCategoryID == _sc.uuid
+                                        ? Colors.black54
+                                        : CustomColors.black),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  );
-                },
+                  ],
+                ),
               ),
             ),
             _subCategoryID.isEmpty
